92dc935f2061c7582c503968da7edeeead7d6f0c
kristintaylor <kristin08taylor@gmail.com>
2015-04-13 22:36:21 -0600

    setup pycharm added exercise 1

---
+++ exercise1.py
@@ -0,0 +1,9 @@
+number_1 = int(raw_input("please enter number: "))
+number_2 = int(raw_input("please enter another number: "))
+
+print number_1 * number_2
+print number_2 - number_1
+print number_1 + number_2
+print number_1 / number_2
+print number_1 % number_2
+
